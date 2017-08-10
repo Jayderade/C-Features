f7ef11d8bcb50ff439c8d5ade5415c36e1e443e2
Jayderade <jcastleden@gmail.com>
2017-08-10 10:10:04 +1000

    Initial Commit

---
+++ Assets/1-Variables/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Variables
+{
+
+    public class Movement : MonoBehaviour
+    {
+        public float movementSpeed = 20f;
+        public float rotationSpeed = 2f;
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Get Horizontal input
+            float inputH = Input.GetAxis("Horizontal");
+            float inputV = Input.GetAxis("Vertical");
+            // Direction X Input(sign) X Speed X DeltaTime
+            transform.position += transform.right * inputV * movementSpeed * Time.deltaTime;
+            // Direction X Input(sign) X Speed X DeltaTime
+            transform.eulerAngles += Vector3.back * inputH * rotationSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/2-Functions/Scripts/Shooting.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Functions
+{
+
+    public class Shooting : MonoBehaviour
+    {
+        // Recoil for the player
+        public float recoil = 30f;
+        // Stores the object we want to Instantiate
+        public GameObject projectPrefab;
+        // Speed at which the projectile will be flung
+        public float projectileSpeed = 20f;
+        // Rate of fire
+        public float shootRate = .1f;
+
+        // Timer to count up to shootRate
+        private float shootTimer = 0f;
+        // Container for the Rigidbody2D
+        private Rigidbody2D rigid;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            // Get rigidbody component
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Increase the timer
+            shootTimer += Time.deltaTime;
+            // AND                               = '&&'
+            // OR                                = '||'
+            // Greater than or equal to          = '>='
+            // Less than or equal to             = '<='
+            // Greater than                      = '>'
+            // Less than                         = '<'
+            // Not equal to                      = '!='
+            // Is equal to                       = '=='
+
+            // IF space bar is pressed AND shootTimer >= shootrate
+                    
+            
+            if (Input.GetKey(KeyCode.Space) && shootTimer >= shootRate)
+            {
+                // CALL Shoot()
+                Shoot();
+                // RESET shootTimer = 0
+                shootTimer = 0;
+            }
+        }
+
+        void Shoot()
+        {
+            // Instantiate a new copy of projectilePrefab
+            GameObject projectile = Instantiate(projectPrefab);
+            // Position of projectile = transform position
+            projectile.transform.position = transform.position;
+            // Apply a force to the projectile
+            Rigidbody2D projectileRigid = projectile.GetComponent<Rigidbody2D>();
+            projectileRigid.AddForce(transform.right * projectileSpeed, ForceMode2D.Impulse);
+            // Apply a recoil to the player
+            rigid.AddForce(-transform.right * recoil, ForceMode2D.Impulse);
+
+        }
+    }
+}
