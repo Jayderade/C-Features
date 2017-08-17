f8cace8d1de6d050efdccc1e9d9aba2c5b22f165
Jayderade <jcastleden@gmail.com>
2017-08-17 11:55:38 +1000

    Pool Game

---
+++ Assets/~Billiards/Scripts/Cue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cue : MonoBehaviour {
+
+    public float gravity = -9.81f;
+
+    private Rigidbody rigid;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid.velocity += Vector3.back * gravity;
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.velocity -= Vector3.back * gravity;
+    }
+
+    void LateUpdate()
+    {
+        rigid.velocity += Vector3.back * gravity;
+    }
+}
