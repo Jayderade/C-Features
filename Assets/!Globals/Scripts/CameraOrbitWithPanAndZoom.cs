14d0f67d4be5e8b6fc9f06ea89522c220efa87e4
Jayderade <jcastleden@gmail.com>
2017-09-22 13:48:46 +1000

    Import balldrop

--- Assets/!Globals/Scripts/CameraOrbitWithPanAndZoom.cs
+++ Assets/!Globals/Scripts/CameraOrbitWithPanAndZoom.cs
@@ -1,40 +1,67 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CameraOrbitWithPanAndZoom : MonoBehaviour
 {
-    public Transform target;
-    public float panSpeed = 5f;
-    public float sensitivity = 1f;
 
+    public Transform target; // Target object to orbit around
+    public float panSpeed = 10f; // Speed of panning
+    public float sensitivity = 1f; // Sensitivity of mouse
+
+
+    // Minimum & maximum zoom distance
     public float distanceMin = .5f;
     public float distanceMax = 15f;
 
-    private float distance = 0f;
+    private float distance = 0f; //Current distance between target amd camera
+    // Stored X & Y euler rotation
     private float x = 0.0f;
     private float y = 0.0f;
 
+    // Stored an enum to use for mouse input (just for readability)
     public enum MouseButton
     {
         LEFTMOUSE = 0,
         RIGHTMOUSE = 1,
         MIDDLEMOUSE = 2,
     }
-
+    // Use this for initialization
     void Start()
     {
-        
+        target.transform.SetParent(null);
+
         distance = Vector3.Distance(target.position, transform.position);
+
         Vector3 angles = transform.eulerAngles;
         x = angles.x;
         y = angles.y;
-        HideCursor(true);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(Input.GetMouseButton((int)MouseButton.RIGHTMOUSE))
+        {
+            HideCursor(true);
+            Orbit();
+        }
+        else if(Input.GetMouseButton((int)MouseButton.MIDDLEMOUSE))
+        {
+            HideCursor(true);
+            Pan();
+        }
+        else
+        {
+            HideCursor(false);
+        }
+        Movement();
     }
 
     void Orbit()
     {
-        x -= Input.GetAxis("Mouse Y") * sensitivity;
-        y += Input.GetAxis("Mouse X") * sensitivity;
+        x = x + Input.GetAxis("Mouse Y") * sensitivity;
+        y = y - Input.GetAxis("Mouse X") * sensitivity;
     }
 
     void Movement()
@@ -42,6 +69,7 @@ public class CameraOrbitWithPanAndZoom : MonoBehaviour
         if (target != null)
         {
             Quaternion rotation = Quaternion.Euler(x, y, 0);
+
             float desiredDist = distance - Input.GetAxis("Mouse ScrollWheel");
             desiredDist = desiredDist * sensitivity;
 
@@ -56,7 +84,18 @@ public class CameraOrbitWithPanAndZoom : MonoBehaviour
             transform.position = position;
         }
     }
-    
+
+    void Pan()
+    {
+        float inputX = -Input.GetAxis("Mouse X");
+        float inputY = -Input.GetAxis("Mouse Y");
+
+        Vector3 inputDir = new Vector3(inputX, inputY);
+
+        Vector3 movement = transform.TransformDirection(inputDir);
+        target.transform.position += movement * panSpeed * Time.deltaTime;
+    }
+
     void HideCursor(bool isHiding)
     {
         if (isHiding)
@@ -69,12 +108,6 @@ public class CameraOrbitWithPanAndZoom : MonoBehaviour
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
-    }
-
-    void LateUpdate()
-    {
-        Orbit();
-        Movement();
     }
 
 }
---
+++ Assets/~AHundredBalls/Scripts/SpawnerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+
+    public GameObject[] prefabs = null;
+    public float spawnRadius = 5f;
+    public float spawnRate = 1f;
+    private float spawnFactor = 0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleSpawn();
+    }
+
+    void HandleSpawn()
+    {
+        spawnFactor += Time.deltaTime;
+        if (spawnFactor > spawnRate)
+        {
+            int randomTimer = Random.Range(0, prefabs.Length);
+            Spawn(prefabs[randomTimer]);
+            spawnFactor = 0;
+        }
+    }
+
+    void Spawn(GameObject _object)
+    {
+        GameObject newObject = Instantiate(_object);
+        Vector3 randomPoint = Random.insideUnitCircle * spawnRadius;
+        newObject.transform.position = transform.position + randomPoint;
+    }
+}
