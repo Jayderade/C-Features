9625627629ab4e0ab07d60d48aaf6550f90ece2f
Jayderade <jcastleden@gmail.com>
2017-10-10 16:05:47 +1100

    Throbbing

---
+++ Assets/8-Inheritance/Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace Inheritance
+{
+    public class Enemy : MonoBehaviour
+    {
+        [Header("Enemy")]
+        public Transform target;
+        public int health = 100;
+        public int damage = 20;
+        public float attackRange = 2f;
+        public float attackRate = 0.5f;
+
+        private Rigidbody rigid;
+        private float attackTimer = 0f;
+        private NavMeshAgent nav;
+
+        void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        public virtual void Attack()
+        {
+            print("Ya Dun Goofed");
+        }              
+                
+        void Update()
+        {
+            // Increase Attack timer
+            attackTimer += Time.deltaTime;
+            // Get distance from enemy to target
+            float distance = Vector3.Distance(transform.position, target.position);
+            // IF distance < attack range
+            if(distance < attackRange)
+            {
+                // Call Attack()
+                Attack();
+                // reset attack timer
+                attackTimer = 0f;
+            }
+            // IF target != null
+            if(target != null)
+            {
+                // Navigate to target
+                nav.SetDestination(target.position);
+            }
+        }
+    }
+}
