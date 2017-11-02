4564c1e73906740b7e0c8d55fc08866b04ef2fc1
Jayderade <jcastleden@gmail.com>
2017-11-02 11:57:49 +1100

    Mario

---
+++ Assets/~Platformer2D/Scripts/Controller2D.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer2D
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Controller2D : MonoBehaviour
+    {
+
+        public float accelerate = 20f;
+        public float jumpHeight = 10f;
+        public float rayDistance = 1f;
+        public LayerMask hitLayers;
+        public bool isGrounded = false;
+
+        private Rigidbody2D rigid2D;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid2D = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            // Perform the raycast and only detect for ground 
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, rayDistance, hitLayers);
+            // Detect if ray hit something
+            if (hit.collider != null)
+            {
+                // Set isGround to true when you are grounded
+                isGrounded = true;
+            }
+            else
+            {
+                // Set isGrounded to false when you are NOT grounded
+                isGrounded = false;
+            }
+        }
+
+
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawLine(transform.position, transform.position + -transform.up * rayDistance);
+        }
+
+        public void Move(float inputH)
+        {
+            rigid2D.AddForce(transform.right * inputH * accelerate);
+        }
+
+        public void Jump()
+        {
+            rigid2D.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+            
+        }
+
+        
+    }
+}
